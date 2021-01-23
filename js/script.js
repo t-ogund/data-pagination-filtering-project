f71e09ff15c6ac2eacd8e09f1eaa6803d1fed304
Toye Ogundepo <ogundepo@gmail.com>
2021-01-22 20:52:21 -0600

    Pagination
    Achieved desired pagination effect.

--- js/script.js
+++ js/script.js
@@ -14,71 +14,65 @@ For assistance:
 
 /*
 Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
+This function creates and appends all the elements needed to display a page.
 */
 
-// const page = null;
 function showPage(list, page) {
-   const startIndex = (page * list.length) - list.length;
-   const endIndex = (page * list.length);
+   const startIndex = (page * 9) - 9;
+   const endIndex = (page * 9);
    const studentListUL = document.querySelector(".student-list");
    studentListUL.innerHTML = "";
-   // console.log(studentListUL)
    for (let i = 0; i < list.length; i++) {
       if (i >= startIndex && i < endIndex) {
-         studentListUL.insertAdjacentHTML("beforeend", `<li class="student-item cf">
-         <div class="student-details">
-            <img class="avatar" src="https://randomuser.me/api/portraits/women/25.jpg" alt="Profile Picture">
-            <h3>Ethel Dean</h3>
-            <span class="email">ethel.dean@example.com</span>
-         </div>
-         <div class="joined-details">
-            <span class="date">Joined 12-15-2005</span>
-         </div>
-      </li>
-      `);
+         studentListUL.insertAdjacentHTML("beforeend", 
+            `<li class="student-item cf">
+               <div class="student-details">
+                  <img class="avatar" src="${data[i].picture.thumbnail}" alt="Profile Picture">
+                  <h3>${data[i].name.first} ${data[i].name.last}</h3>
+                  <span class="email">${data[i].email}</span>
+               </div>
+               <div class="joined-details">
+                  <span class="date">Joined ${data[i].registered.date}</span>
+               </div>
+            </li>`
+         );
       }
    }
 }
 
 
-
-
-
 /*
 Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
+This function creates the buttons necessary to navigate between pages
 */
 function addPagination(list) {
    const numberOfButtons = Math.ceil(list.length / 9);
    const linkListUL = document.querySelector(".link-list");
    linkListUL.innerHTML = "";
    for (let i = 0; i < numberOfButtons; i++) {
-   //    linkListUL.insertAdjacentHTML("beforeend", `<li>
-   //    <button type="button>${i}</button>
-   // </li>`)
-      
       const li = document.createElement("li");
       const button = document.createElement("button");
       button.textContent = i + 1;
       linkListUL.appendChild(li);
       li.appendChild(button);
    }
-   // console.log(numberOfButtons);
-   // numberOfButtons.className = "active";
-   // linkListUL[0].addEventListener("click", function() {
-   //    console.log("hello");
-   // })
+   
    const firstButton = linkListUL.children[0].childNodes[0];
    firstButton.className = "active";
 
-   
-
+   linkListUL.addEventListener("click", function(e) {
+      if (e.target.tagName == "BUTTON") {
+         for (let i = 0; i < linkListUL.children.length; i++) {
+            const pageButton = linkListUL.children[i].children[0]
+            pageButton.classList.remove("active");
+         }
+         e.target.classList.add("active");
+         showPage(list, e.target.textContent)
+      }
+   })
 }
 
 
-
-
-// Call functions
+// This is the section where the functions are called
 showPage(data, 1);
 addPagination(data);
